5a2513458255dcc1896af4177c210992f381f14e
[QA] Michel Mendoza <michel.mendoza@voltbras.com.br>
2024-09-07 16:15:26 -0300

    feat: compiler changes

---
+++ src/server.js
@@ -0,0 +1,35 @@
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const fastify_1 = __importDefault(require("fastify"));
+const cors_1 = __importDefault(require("@fastify/cors"));
+const routes_1 = require("./routes");
+const dotenv_1 = __importDefault(require("dotenv"));
+dotenv_1.default.config();
+const porta = process.env.PORT || 3000;
+const app = (0, fastify_1.default)({ logger: true });
+const start = () => __awaiter(void 0, void 0, void 0, function* () {
+    app.setErrorHandler((error, request, reply) => {
+        reply.code(400).send({ message: error.message });
+    });
+    yield app.register(routes_1.routes);
+    yield app.register(cors_1.default);
+    try {
+        yield app.listen({ port: 3000 });
+    }
+    catch (err) {
+        process.exit(1);
+    }
+});
+start();
